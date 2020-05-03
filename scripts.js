c0f9e85cdb1a9a19d4935e43bd60879180f82559
Fitnessprogrammer <48212219+fitnessprogrammer@users.noreply.github.com>
2020-05-03 17:56:53 +0200

    finishing js examples

--- scripts.js
+++ scripts.js
@@ -14,12 +14,15 @@ function addLike(){
 $('#like').click(addLike);
 
 function myLittleFunction() {
-    $('main').append('<p>A p is appended in the main.</p>');
+    if ($('p').length < 5) {
+        $('main').append('<p>A p is appended in the main.</p>');
+    } else {
+        console.log('We have reached the max. paragraph number!');
+    }
     $('p').css('border', '1px solid red');
     $('p').css('padding', '10px');
 }
 
-myLittleFunction();
 myLittleFunction();
 
 $('#add').click(myLittleFunction);
@@ -27,6 +30,21 @@ $('#remove').click(function () {
     $('p:last-child').remove()
 });
 
+let colors = ['orange', 'red', 'blue', 'green', 'black'];
+
+colors.forEach(function (color) {
+    $('#box-container').append('<div class="box"></div>');
+    $('.box:last-child').css('background',color);
+});
+
+for (let i = 0; i < 112; i++) {
+console.log("I'll not give up!"); 
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
 
 let name = 'Gabor'
 let age = 27;
