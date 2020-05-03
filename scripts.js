7dcdadf40665a600dcba2afa30739a6607f80d47
Fitnessprogrammer <48212219+fitnessprogrammer@users.noreply.github.com>
2020-05-03 13:54:07 +0200

    like button, p button

--- scripts.js
+++ scripts.js
@@ -1,8 +1,34 @@
 console.log('Script file is loaded');
 $('h1').text('This is done by JavaScript');
-$('p').css('border', '1px solid red');
-$('p').css('padding', '10px');
 $('h2').toggleClass('blue');
 
+var number = 1;
+
+function addLike(){
+    var count = document.getElementById('count');
+    number++;
+    count.innerHTML = number;
+}
+
+
+$('#like').click(addLike);
+
+function myLittleFunction() {
+    $('main').append('<p>A p is appended in the main.</p>');
+    $('p').css('border', '1px solid red');
+    $('p').css('padding', '10px');
+}
+
+myLittleFunction();
+myLittleFunction();
+
+$('#add').click(myLittleFunction);
+$('#remove').click(function () {
+    $('p:last-child').remove()
+});
+
+
+let name = 'Gabor'
+let age = 27;
 
 // $('css selector').action()
