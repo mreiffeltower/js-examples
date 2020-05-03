7e2f1b5b98e19a991b6a1fc59fa88f165ee26e53
Fitnessprogrammer <48212219+fitnessprogrammer@users.noreply.github.com>
2020-05-03 10:59:05 +0200

    jquery first examples

---
+++ scripts.js
@@ -0,0 +1,8 @@
+console.log('Script file is loaded');
+$('h1').text('This is done by JavaScript');
+$('p').css('border', '1px solid red');
+$('p').css('padding', '10px');
+$('h2').toggleClass('blue');
+
+
+// $('css selector').action()
